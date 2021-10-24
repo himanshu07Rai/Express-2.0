b235e22712a86792b91939d3a3e56d022287ce77
Himanshu Rai <raihimanshu22345@gmail.com>
2021-10-24 10:15:33 +0530

    default middlewares

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
